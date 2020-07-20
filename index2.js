285dfd3bd5c4da8a04e1afd8aa75502ad94e8bc3
Vivekvar <63313913+Vivekvar@users.noreply.github.com>
2020-07-20 21:04:06 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const tasks = [
+    { id: 1, name: 'Nodejs' },
+    { id: 2, name: 'Javascript' },
+    { id: 3, name: 'Expressjs' },
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+app.get('/api/tasks', (req, res) => {
+    res.send(tasks);
+});
+
+app.get('/api/tasks/:id', (req, res) => {
+    var task = tasks.find(c => c.id === parseInt(req.params.id));
+    if (!task) {
+        res.status(404).send('The given task ID does not exists');
+        return;
+    }
+    res.send(task);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000....'));
