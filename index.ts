3c7e0d558d2bfb8eaee94acc8ff95ac6bfa7b27b
Vivekvar <63313913+Vivekvar@users.noreply.github.com>
2020-08-09 14:12:20 +0530

    Delete index.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ping.controller';
-export * from './hello.controller';
-export * from './todo.controller';
-export * from './todo-list-todo.controller';
-export * from './todo-todo-list.controller';
